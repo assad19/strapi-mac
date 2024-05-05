b70bff457970fe52315f295d1d5b4254f61bdc9d
assad19 <154305824+assad19@users.noreply.github.com>
2024-05-05 17:16:17 +0500

    Thrid change
    this is the second comit

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['gex36xqLvZ8lQbgmwzqCTQ==,XHtoNPDDUzdZkeK0LlZljA==,4dDxfyonDsE2KaQvh9AEKQ==,SfYZtaxgsrZNr7E978w+6w==', '39HMD3SN5MghjKZVuo0jgQ=='], // Define your keys here
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
